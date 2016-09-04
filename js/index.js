fd9c5d1c0021051bbd616f9f8a71de25961e3493
yasic <2207793765@qq.com>
2016-09-04 11:04:17 +0800

    first push

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var iconArr = document.querySelectorAll('#cf-intro .landing-skill-icon,#cf-intro .img-awesome-padding');
-    iconArr.forEach(function (el) {
+    /*iconArr.forEach(function (el) {
         el.className += ' animated';
         el.addEventListener("mouseover", function () {
             if (this.className.indexOf('flip') === -1) {
@@ -10,63 +10,65 @@ window.onload = function () {
         el.addEventListener("webkitAnimationEnd", function () { //动画结束时事件
             this.className = this.className.replace('flip', ' ');
         })
-    });
+    });*/
+
+    var members = [
+        {
+            "name": "余烜",
+            "imgurl": "https://avatars2.githubusercontent.com/u/11681135?v=3&s=460",
+            "url": ""
+        },
+        {"name": "王嘉琨", "imgurl": "./img/members/wangjiakun.jpg"},
+        {"name": "肖鹏", "imgurl": "./img/members/xiaopeng.jpg"},
+        {"name": "姜玉珍", "imgurl": "./img/members/jiangyuzhen.jpg"},
+        {"name": "李春霖", "imgurl": "./img/members/lichunlin.jpg"},
+        {"name": "刘小东", "imgurl": "./img/members/liuxiaodong.jpg"},
+        {"name": "魏杨", "imgurl": "./img/members/weiyang.jpg"},
+        {"name": "赵小强", "imgurl": "./img/members/zhaoxiaoqiang.jpg"},
+        {"name": "肖彰瑾", "imgurl": "./img/members/xiaozhangjin.jpg"},
+        {"name": "YaYa", "imgurl": "./img/members/yaya.jpg"},
+        {"name": "凡小东", "imgurl": "./img/members/fanxiaodong.jpg"},
+        {"name": "吉祥", "imgurl": "./img/members/jixiang.jpg"},
+        {"name": "阿KinG", "imgurl": "./img/members/aking.jpg"},
+        {"name": "Quiz42小考", "imgurl": "./img/members/quiz42xiaokao.jpg"},
+        {"name": "王丹文", "imgurl": "./img/members/wangdanwen.jpg"},
+        {"name": "levimin", "imgurl": "./img/members/levimin.jpg"},
+        {"name": "陈科", "imgurl": "./img/members/chenke.jpg"},
+        {"name": "莉莉安", "imgurl": "./img/members/lilian.jpg"},
+        {"name": "吴其凡", "imgurl": "./img/members/wuqifan.jpg"},
+        {"name": "周真", "imgurl": "./img/members/zhouzhen.jpg"},
+        {"name": "李欣", "imgurl": "./img/members/lixin.jpg"},
+        {"name": "袁伟", "imgurl": "./img/members/yuanwei.jpg"},
+        {"name": "陈曦", "imgurl": "./img/members/chenxi.jpg"},
+        {
+            "name": "Aperture leaf",
+            "imgurl": "./img/members/apertureleaf.jpg"
+        },
+        {"name": "恩娇", "imgurl": "./img/members/enjiao.jpg"},
+        {"name": "莱昂", "imgurl": "./img/members/laiang.jpg"},
+        {"name": "徐顺安", "imgurl": "./img/members/xushunan.jpg"},
+        {"name": "unofficial", "imgurl": "./img/members/unofficial.jpg"},
+        {"name": "书香剑墨", "imgurl": "./img/members/shuxiangmojian.jpg"},
+        {"name": "我是小禾", "imgurl": "./img/members/woshixiaohe.jpg"},
+        {"name": "king", "imgurl": "./img/members/king.jpg"},
+        {"name": "晋剑", "imgurl": "./img/members/jinjian.jpg"},
+        {"name": "Miya", "imgurl": "./img/members/miya.jpg"},
+        {"name": "王峰", "imgurl": "./img/members/wangfeng.jpg"}
+    ];
+
+    function addHtml(o) {
+        var html = '<div class="col-sm-3">' +
+            '<div class="thumbnail member">' +
+            '<img src=' + o.imgurl + ' alt="">' +
+            '<div class="caption text-center">' +
+            ' <h3>' + o.name + '</h3>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+        $(html).appendTo($(".members"));
+    }
+
     for (var i = 0; i < members.length; i++) {
         addHtml(members[i]);
     }
-}
-var members = [
-    {
-        "name": "余烜",
-        "imgurl": "./img/members/yuxuan.jpg",
-        "url": ""
-    },
-    {"name": "王嘉琨", "imgurl": "./img/members/wangjiakun.jpg"},
-    {"name": "肖鹏", "imgurl": "./img/members/xiaopeng.jpg"},
-    {"name": "姜玉珍", "imgurl": "./img/members/jiangyuzhen.jpg"},
-    {"name": "李春霖", "imgurl": "./img/members/lichunlin.jpg"},
-    {"name": "刘小东", "imgurl": "./img/members/liuxiaodong.jpg"},
-    {"name": "魏杨", "imgurl": "./img/members/weiyang.jpg"},
-    {"name": "赵小强", "imgurl": "./img/members/zhaoxiaoqiang.jpg"},
-    {"name": "肖彰瑾", "imgurl": "./img/members/xiaozhangjin.jpg"},
-    {"name": "YaYa", "imgurl": "./img/members/yaya.jpg"},
-    {"name": "凡小东", "imgurl": "./img/members/fanxiaodong.jpg"},
-    {"name": "吉祥", "imgurl": "./img/members/jixiang.jpg"},
-    {"name": "阿KinG", "imgurl": "./img/members/aking.jpg"},
-    {"name": "Quiz42小考", "imgurl": "./img/members/quiz42xiaokao.jpg"},
-    {"name": "王丹文", "imgurl": "./img/members/wangdanwen.jpg"},
-    {"name": "levimin", "imgurl": "./img/members/levimin.jpg"},
-    {"name": "陈科", "imgurl": "./img/members/chenke.jpg"},
-    {"name": "莉莉安", "imgurl": "./img/members/lilian.jpg"},
-    {"name": "吴其凡", "imgurl": "./img/members/wuqifan.jpg"},
-    {"name": "周真", "imgurl": "./img/members/zhouzhen.jpg"},
-    {"name": "李欣", "imgurl": "./img/members/lixin.jpg"},
-    {"name": "袁伟", "imgurl": "./img/members/yuanwei.jpg"},
-    {"name": "陈曦", "imgurl": "./img/members/chenxi.jpg"},
-    {
-        "name": "Aperture leaf",
-        "imgurl": "./img/members/apertureleaf.jpg"
-    },
-    {"name": "恩娇", "imgurl": "./img/members/enjiao.jpg"},
-    {"name": "莱昂", "imgurl": "./img/members/laiang.jpg"},
-    {"name": "徐顺安", "imgurl": "./img/members/xushunan.jpg"},
-    {"name": "unofficial", "imgurl": "./img/members/unofficial.jpg"},
-    {"name": "书香剑墨", "imgurl": "./img/members/shuxiangmojian.jpg"},
-    {"name": "我是小禾", "imgurl": "./img/members/woshixiaohe.jpg"},
-    {"name": "king", "imgurl": "./img/members/king.jpg"},
-    {"name": "晋剑", "imgurl": "./img/members/jinjian.jpg"},
-    {"name": "Miya", "imgurl": "./img/members/miya.jpg"},
-    {"name": "王峰", "imgurl": "./img/members/wangfeng.jpg"}
-];
-
-function addHtml(o) {
-    var html = '<div class="col-sm-3">' +
-        '<div class="thumbnail member">' +
-        '<img src=' + o.imgurl + ' alt="">' +
-        '<div class="caption text-center">' +
-        ' <h3>' + o.name + '</h3>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
-    $(html).appendTo($(".members"));
 }
---
+++ js/member.animation.js
@@ -0,0 +1,165 @@
+/**
+ * Created by yasic on 16-9-3.
+ */
+
+var data = {
+    "nodes" : [
+        {"name": "余烜", "imgurl": "https://avatars2.githubusercontent.com/u/11681135?v=3&s=460",},
+        {"name": "王嘉琨", "imgurl": "./img/members/wangjiakun.jpg"},
+        {"name": "肖鹏", "imgurl": "./img/members/xiaopeng.jpg"},
+        {"name": "姜玉珍", "imgurl": "./img/members/jiangyuzhen.jpg"},
+        {"name": "李春霖", "imgurl": "./img/members/lichunlin.jpg"},
+        {"name": "魏杨", "imgurl": "./img/members/weiyang.jpg"},
+        {"name": "赵小强", "imgurl": "./img/members/zhaoxiaoqiang.jpg"},
+        {"name": "凡小东", "imgurl": "./img/members/fanxiaodong.jpg"},
+        {"name": "吉祥", "imgurl": "./img/members/jixiang.jpg"},
+        {"name": "Quiz42小考", "imgurl": "./img/members/quiz42xiaokao.jpg"},
+        {"name": "王丹文", "imgurl": "./img/members/wangdanwen.jpg"},
+        {"name": "levimin", "imgurl": "./img/members/levimin.jpg"},
+        {"name": "陈科", "imgurl": "./img/members/chenke.jpg"},
+        {"name": "莉莉安", "imgurl": "./img/members/lilian.jpg"},
+        {"name": "李欣", "imgurl": "./img/members/lixin.jpg"},
+        {"name": "陈曦", "imgurl": "./img/members/chenxi.jpg"},
+        {"name": "恩娇", "imgurl": "./img/members/enjiao.jpg"},
+        {"name": "晋剑", "imgurl": "./img/members/jinjian.jpg"},
+        {"name": "Miya", "imgurl": "./img/members/miya.jpg"},
+        {"name": "王峰", "imgurl": "./img/members/wangfeng.jpg"}
+    ],
+    "links": [
+        {"source":1,"target":0},    {"source":2,"target":1},
+        {"source":3,"target":2},    {"source":4,"target":3},
+        {"source":5,"target":4},    {"source":6,"target":0},
+        {"source":7,"target":6},    {"source":8,"target":7},
+        {"source":9,"target":8},    {"source":10,"target":9},
+        {"source":11,"target":0},    {"source":12,"target":11},
+        {"source":13,"target":12},    {"source":14,"target":13},
+        {"source":15,"target":14},    {"source":16,"target":0},
+        {"source":17,"target":16},    {"source":18,"target":17},
+        {"source":19,"target":18},
+    ]
+}
+
+var width = 1500, height = 900;
+
+var svg = d3.select("body").select(".members").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+
+var force = d3.layout.force()
+    .size([width, height])
+    .linkDistance(700)
+    .charge([-400])
+    .nodes(data.nodes)
+    .links(data.links)
+    .start();
+
+var color=d3.scale.category20c();
+
+/*var index = 0;
+
+var defs = svg.selectAll("defs")
+    .data(data.nodes)
+    .enter()
+    .append("defs");
+
+var rects = defs.selectAll("rect")
+    .data([1])
+    .enter()
+    .append("rect")
+    .attr("id", function(d){return "rect-" + d.name})
+    .attr("x", "25%")
+    .attr("y", "15%")
+    .attr("width", img_width)
+    .attr("height", img_height)
+    .attr("rx", "100");
+
+var clipPath = rects.selectAll("clipPath")
+    .data([1])
+    .enter()
+    .append("clipPath")
+    .attr("width", img_width)
+    .attr("height", img_height);
+
+clipPath.selectAll("use")
+    .data(data.nodes)
+    .enter()
+    .append("use")
+    .attr("xlink:href", function (d) {
+        return "#" + d.name;
+    });*/
+
+var node = d3.select("members-city")
+    .append("svg:image")
+    .attr("class", "circle").attr("xlink:href", "https://avatars2.githubusercontent.com/u/11681135?v=3&s=460")
+    .attr("x", "20px").attr("y", "20px")
+    .attr("width", "16px")
+    .attr("height", "16px");
+
+var link = svg.selectAll(".link")
+    .data(data.links)
+    .enter()
+    .append("line")
+    .style("stroke","#ccc")
+    .style("stroke-width",1)
+    ;
+
+
+/*var node = svg.selectAll(".node")
+    .data(data.nodes)
+    .enter()
+    .append("circle")
+    .attr("fill", function(d,i){ return color(i);})
+    .attr("r", 5)
+    .attr("stroke","black")
+    .attr("stroke-width",1).call(force.drag());*/
+
+var img_width = 50;
+var img_height = 50;
+
+var nodes_img = svg.selectAll("image")
+    .data(data.nodes)
+    .enter()
+    .append("image")
+    .attr("width", img_width)
+    .attr("height",img_height)
+    .attr("class", "member-image")
+    .attr("xlink:href",function(d){
+        return d.imgurl;
+    })
+    .attr("shape", "circle")
+    .call(force.drag);
+
+var text_dx = 20;
+var text_dy = 20;
+
+var nodes_text = svg.selectAll(".nodetext")
+    .data(data.nodes)
+    .enter()
+    .append("text")
+    .attr("class","nodetext")
+    .attr("dx",text_dx)
+    .attr("dy",text_dy)
+    .text(function(d){
+        return d.name;
+    });
+
+force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+
+    nodes_img.attr("x",function(d){ return d.x - img_width/2; });
+    nodes_img.attr("y",function(d){ return d.y - img_height/2; });
+
+    nodes_text.attr("x",function(d){ return d.x });
+    nodes_text.attr("y",function(d){ return d.y + img_height/2; });
+
+    /*defs.attr("x",function(d){ return d.x - img_width/2 });
+    defs.attr("y",function(d){ return d.y + img_height/2; })*/
+
+});
