3de663a0b6c34f2710ae07d8c10180ac1fea0cc5
Tech_Query <shiy007@qq.com>
2017-02-20 19:39:34 +0800

    【重构】基于 AMD 规范整理 JS 代码，提升可维护性、加载性能
    【修正】“学习技能”图标动画 PC 可用    #1

--- js/member.animation.js
+++ js/member.animation.js
@@ -1,30 +1,15 @@
 /**
  * Created by yasic on 16-9-3.
+ *
+ * Optimized by TechQuery on 17-2-20
  */
-if (window.screen.availWidth > 900){
+
+define(['d3', 'members'],  function (d3, members) {
+
+    if (window.screen.availWidth < 900)  return;
+
     var data = {
-        "nodes" : [
-            {"name": "余烜Yasic", "imgurl": "https://avatars2.githubusercontent.com/u/11681135?v=3&s=460",},
-            {"name": "王嘉琨", "imgurl": "./img/members/wangjiakun.jpg"},
-            {"name": "肖鹏", "imgurl": "./img/members/xiaopeng.jpg"},
-            {"name": "姜玉珍", "imgurl": "./img/members/jiangyuzhen.jpg"},
-            {"name": "李春霖", "imgurl": "./img/members/lichunlin.jpg"},
-            {"name": "魏杨", "imgurl": "./img/members/weiyang.jpg"},
-            {"name": "赵小强", "imgurl": "./img/members/zhaoxiaoqiang.jpg"},
-            {"name": "凡小东", "imgurl": "./img/members/fanxiaodong.jpg"},
-            {"name": "吉祥", "imgurl": "./img/members/jixiang.jpg"},
-            {"name": "Quiz42小考", "imgurl": "./img/members/quiz42xiaokao.jpg"},
-            {"name": "王丹文", "imgurl": "./img/members/wangdanwen.jpg"},
-            {"name": "levimin", "imgurl": "./img/members/levimin.jpg"},
-            {"name": "陈科", "imgurl": "./img/members/chenke.jpg"},
-            {"name": "莉莉安", "imgurl": "./img/members/lilian.jpg"},
-            {"name": "李欣", "imgurl": "./img/members/lixin.jpg"},
-            {"name": "陈曦", "imgurl": "./img/members/chenxi.jpg"},
-            {"name": "恩娇", "imgurl": "./img/members/enjiao.jpg"},
-            {"name": "晋剑", "imgurl": "./img/members/jinjian.jpg"},
-            {"name": "Miya", "imgurl": "./img/members/miya.jpg"},
-            {"name": "王峰", "imgurl": "./img/members/wangfeng.jpg"}
-        ],
+        "nodes" : members.list,
         /*"links": [
          {"source":1,"target":0},    {"source":2,"target":1},
          {"source":3,"target":2},    {"source":4,"target":3},
@@ -188,6 +173,5 @@ if (window.screen.availWidth > 900){
 
         /*defs.attr("x",function(d){ return d.x - img_width/2 });
          defs.attr("y",function(d){ return d.y + img_height/2; })*/
-
     });
-}
+});
