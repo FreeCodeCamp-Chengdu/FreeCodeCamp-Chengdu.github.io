75cbb6bbd36972438efa56c790736ee6a56e1be7
TechQuery <shiy007@qq.com>
2019-04-06 04:26:31 +0800

    [ Add ]  File List widget of Post layout [ Optimize ]  Several details

--- themes/Icarus/includes/helpers/data.js
+++ themes/Icarus/includes/helpers/data.js
@@ -1,11 +1,39 @@
-const { VEvent } = require('icalendar');
+const { VEvent } = require('icalendar'),
+  type_map = {
+    doc:  'word',
+    docx: 'word',
+    xls:  'excel',
+    xlsx: 'excel',
+    ppt:  'powerpoint',
+    pptx: 'powerpoint'
+  };
 
 
 module.exports = function (hexo) {
 
+  hexo.extend.helper.register('has_category',  (post, name) =>
+
+    Array.from((post.categories || '').data,  ({ name }) => name).includes( name )
+  );
+
+  hexo.extend.helper.register('file_type',  path => {
+
+    var type = path.split('.');
+
+    if (! type[1]) return;
+
+    type = type.slice(-1)[0].toLowerCase();
+
+    return type_map[type] || type;
+  });
+
+  hexo.extend.helper.register('toDataURI',  (data, type = '') =>
+
+    `data:${type};base64,${Buffer.from( data ).toString('base64')}`
+  );
+
   hexo.extend.helper.register(
-    'eventOf',
-    function (title, description, start, end, location) {
+    'eventOf',  (title, description, start, end, location) => {
 
       const event = new VEvent();
 
@@ -20,9 +48,4 @@ module.exports = function (hexo) {
       return event;
     }
   );
-
-  hexo.extend.helper.register('toDataURI', function (data, type = '') {
-
-    return `data:${type};base64,${Buffer.from( data ).toString('base64')}`;
-  });
 };
